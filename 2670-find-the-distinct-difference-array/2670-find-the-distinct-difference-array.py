d818db1e16f439c70d690c79a49e1af98b8b51b5
varsha-alumula77 <alumulavarsha77@gmail.com>
2026-07-17 16:37:51 +0530

    Time: 27 ms (9.68%), Space: 19.2 MB (59.53%) - LeetHub

---
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        res=[]
+        for i in range(len(nums)):
+            left=len(set(nums[:i+1]))
+            right=len(set(nums[i+1:]))
+            temp=left-right
+            res.append(temp)
+        return res
+
