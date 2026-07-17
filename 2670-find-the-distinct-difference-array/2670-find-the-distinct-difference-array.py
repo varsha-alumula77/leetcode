cbf8ed2807963fc0fe284f73afc0d302ac609360
varsha-alumula77 <alumulavarsha77@gmail.com>
2026-07-17 16:55:17 +0530

    Time: 42 ms (5.86%), Space: 19.3 MB (59.53%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
@@ -1,10 +1,19 @@
 class Solution:
     def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        i=0
         res=[]
-        for i in range(len(nums)):
-            left=len(set(nums[:i+1]))
-            right=len(set(nums[i+1:]))
-            temp=left-right
+        while i<len(nums):
+            left_ele=[]
+            right_ele=[]
+            for j in range(i+1):
+                if nums[j] not in left_ele:
+                    left_ele.append(nums[j])
+            for k in range(i+1,len(nums)):
+                if nums[k] not in right_ele:
+                    right_ele.append(nums[k])
+            temp=len(left_ele)-len(right_ele)
             res.append(temp)
+            i+=1
         return res
+
 
