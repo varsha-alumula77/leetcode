66187e83137c9945e8f2fe6b38370bc5395a1ec7
varsha-alumula77 <alumulavarsha77@gmail.com>
2026-07-21 18:11:47 +0530

    Time: 4 ms (0.63%), Space: 19.4 MB (27.43%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
@@ -7,13 +7,22 @@ class Solution:
         #     temp=n%2
         #     n=n//2
         # return True
-        lis=[]
-        while n>0:
-            lis.append(n%2)
-            n=n//2
-        for i in range(1,len(lis)):
-            if lis[i-1]==lis[i]:
-                return False
-        return True
 
+        # lis=[]
+        # while n>0:
+        #     lis.append(n%2)
+        #     n=n//2
+        # for i in range(1,len(lis)):
+        #     if lis[i-1]==lis[i]:
+        #         return False
+        # return True
+        
+        prev=-1
+        while n>0:
+            last_value=n&1
+            if last_value==prev:
+                return False
+            prev=last_value
+            n=n>>1
+        return True
         
